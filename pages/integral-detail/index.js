18f26258eca750e03b827cecbd9e96c086e8637d
wangyaju0910 <1832027960@qq.com>
2019-05-21 18:22:25 +0800

    第四天

---
+++ pages/integral-detail/index.js
@@ -0,0 +1,33 @@
+Page({
+  data: {
+    imgUrls: [
+      "../../images/tuceng.png",
+      "../../images/tuceng.png",
+      "../../images/tuceng.png"
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000
+  },
+  changeIndicatorDots(e) {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
+    })
+  },
+  changeAutoplay(e) {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+  intervalChange(e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+  durationChange(e) {
+    this.setData({
+      duration: e.detail.value
+    })
+  }
+})
